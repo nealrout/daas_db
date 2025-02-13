1a245db8e68bee897a92e5ad00c9ee3a1ed1c70c
nealrout <nroutson@gmail.com>
2025-02-12 19:57:22 -0500

    account and facility

--- sql/func_asset.sql
+++ sql/func_asset.sql
@@ -17,10 +17,10 @@ BEGIN
 END;
 ' LANGUAGE plpgsql;
 /
-CALL drop_functions_by_name('get_asset_by_id');
+CALL drop_functions_by_name('get_asset_by_json');
 /
 -- Stored procedure to get an asset by ID
-CREATE OR REPLACE FUNCTION get_asset_by_id(p_jsonb jsonb, p_user_id bigint)
+CREATE OR REPLACE FUNCTION get_asset_by_json(p_jsonb jsonb, p_user_id bigint)
 RETURNS TABLE(acct_nbr text, fac_nbr text, asset_nbr text, sys_id text, status_code text, create_ts timestamptz, update_ts timestamptz)
 AS '
 DECLARE
